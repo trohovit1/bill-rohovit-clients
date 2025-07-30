2828d80cbd3befc11bcc69156a8473c4f8e74564
trohovit1 <trohovi1@jh.edu>
2025-07-29 19:54:21 -0700

    still trying to fix vite problems

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: '/bill-rohovit-clients/',
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/bill-rohovit-clients/',
   // server: {
   //   port: 3000,
   // },
